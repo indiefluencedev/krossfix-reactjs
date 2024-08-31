95766ec4759b960dba9d00e5529e234bdee23128
anurag262000 <anuragmishra262000@gmail.com>
2024-08-31 12:04:57 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-blue-500 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">Hello, Tailwind!</h1>
+    </div>
+  );
+}
