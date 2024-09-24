e04b8393b4ce9f77412a803e94eb04934393d3d7
gaurav23617 <gaurav23617@gmail.com>
2024-09-24 12:26:07 +0530

    "Updated image imports and references in Products2 component"

--- src/Pages/Products2.jsx
+++ src/Pages/Products2.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Banner from '../Components/Banner'; // Adjust the path if necessary
 import InfoSection from '../Components/ProductsPages/InfoSection'; // Adjust the path if necessary
 import bannerImage from '../assets/productsimages/banner.png';
-import ProductsImage from '../../assets/productsimages/Products.png';
+import ProductImage from '../assets/productsimages/products.png' 
 import TableComponent from '../Components/TableComponents'; // Adjust the path if necessary
 
 const Products2 = () => {
@@ -18,7 +18,7 @@ const Products2 = () => {
       {/* Info Section */}
       <div className="max-w-screen-xl mx-auto">
         <InfoSection
-          imageSrc={ProductsImage}
+          imageSrc={ProductImage}
           title="Products 2"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod eleifend massa, nec blandit velit. Proin ut magna ac eros feugiat vehicula.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod eleifend massa, nec blandit velit. Proin ut magna ac eros feugiat vehicula."
         />
